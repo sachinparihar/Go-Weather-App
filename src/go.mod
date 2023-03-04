64795952b94195c57d052181b18f94d969e76e81
sachinparihar <sachinparihar98120@gmail.com>
2023-03-04 23:05:18 +0530

    update go code

--- src/go.mod
+++ src/go.mod
@@ -1,3 +1,3 @@
-module github.com/sachinparihar/Go-Weather-App
+module main
 
 go 1.20
--- src/main.go
+++ src/main.go
@@ -5,82 +5,86 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"strings"
 )
 
-type apiConfigData struct {
-	OpenWeatherMapApiKey string `json:"OpenWeatherMapApiKey`
-}
-
 type weatherData struct {
-	Name string `json:"name`
-	Main struct {
-		Kelvin float64 `json:"temp"`
-	} `json:"main"`
-	Celsius float64 `json:"celsius"`
-}
-
-func loadApiConfig(filename string) (apiConfigData, error) {
-	bytes, err := ioutil.ReadFile(filename)
-
-	if err != nil {
-		return apiConfigData{}, err
-	}
-
-	var c apiConfigData
-
-	err = json.Unmarshal(bytes, &c)
-	if err != nil {
-		return apiConfigData{}, err
-	}
-	return c, nil
-}
-
-func hello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("hello from go!\n"))
-}
-
-func query(city string) (weatherData, error) {
-	apiConfig, err := loadApiConfig(".apiConfig")
-	if err != nil {
-		return weatherData{}, err
-	}
-
-	resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?APPID=" + apiConfig.OpenWeatherMapApiKey + "&q=" + city)
-	if err != nil {
-		return weatherData{}, err
-	}
-
-	defer resp.Body.Close()
-
-	var d weatherData
-	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
-		return weatherData{}, err
-	}
-
-	// Convert Kelvin to Celsius
-	d.Celsius = d.Main.Kelvin - 273.15
-
-	return d, nil
+	City        string  `json:"city"`
+	Temperature float64 `json:"temp"`
+	Humidity    int     `json:"humidity"`
+	Clouds      int     `json:"clouds"`
+	WindSpeed   float64 `json:"wind_speed"`
 }
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "index.html")
-	})
-
-	http.HandleFunc("/weather/",
-		func(w http.ResponseWriter, r *http.Request) {
-			city := strings.SplitN(r.URL.Path, "/", 3)[2]
-			data, err := query(city)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			w.Header().Set("Content-Type", "application/json; charset=utf-8")
-			json.NewEncoder(w).Encode(data)
-		})
-
+	http.HandleFunc("/", handleIndex)
+	http.HandleFunc("/weather/", handleWeather)
 	fmt.Println("Starting server on port 8080")
 	http.ListenAndServe(":8080", nil)
+}
+
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	html, err := ioutil.ReadFile("index.html")
+	if err != nil {
+		http.Error(w, "Error reading file", http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprint(w, string(html))
+}
+
+func handleWeather(w http.ResponseWriter, r *http.Request) {
+	// Extract city name from query parameter
+	city := r.URL.Query().Get("city")
+	if city == "" {
+		fmt.Fprint(w, "Please provide a city name")
+		return
+	}
+
+	// Construct URL to request weather data
+	apiKey := "db9058e7659fb6c7faf51f7523524e45"
+	url := fmt.Sprintf("http://api.openweathermap.org/data/2.5/weather?APPID=%s&q=%s", apiKey, city)
+
+	// Make request to OpenWeatherMap API
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Fprintf(w, "Error fetching weather data: %v", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// Read response body
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Error reading weather data: %v", err)
+		return
+	}
+
+	// Parse JSON response into weatherData struct
+	var data struct {
+		Name string `json:"name"`
+		Main struct {
+			Kelvin   float64 `json:"temp"`
+			Humidity int     `json:"humidity"`
+		} `json:"main"`
+		Clouds struct {
+			All int `json:"all"`
+		} `json:"clouds"`
+		Wind struct {
+			Speed float64 `json:"speed"`
+		} `json:"wind"`
+	}
+	if err := json.Unmarshal(body, &data); err != nil {
+		fmt.Fprintf(w, "Error decoding weather data: %v", err)
+		return
+	}
+
+	// Set city name and return response as JSON
+	weatherData := weatherData{
+		City:        data.Name,
+		Temperature: data.Main.Kelvin - 273.15,
+		Humidity:    data.Main.Humidity,
+		Clouds:      data.Clouds.All,
+		WindSpeed:   data.Wind.Speed,
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(weatherData)
 }
